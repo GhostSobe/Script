822b4c171785fd7555b4b45c65fad2d8c954e542
Ghost <jirapat2957@gmail.com>
2025-08-16 20:38:21 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,38 +1,15 @@
--- Made By Khaotom
-
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
-
-function loadscript()
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = "Notification",
-        Text = "Script Load Succeed",
-        Icon = "rbxassetid://118974663345349",
-        Duration = 3
-    })
-end
 
 if game.PlaceId == 126884695634066 or game.PlaceId == 91867617264223 then -- Grow a Garden
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/GrowaGarden"))();
-    loadscript()
 elseif game.PlaceId == 79546208627805 or game.PlaceId == 126509999114328 then -- 99 Nights in the Forest
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/99%20Nights%20in%20the%20Forest"))();
-    loadscript()
 elseif game.PlaceId == 129827112113663 then -- Prospecting
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/Prospecting"))();
-    loadscript()
 elseif game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))();
-    loadscript()
-elseif game.GameId == 994732206 then -- Blox Fruit
-    local Settings = {
-        JoinTeam = "Pirates";
-        Translator = true;
-    }
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
-    loadscript()
 elseif game.GameId == 7709344486 then -- Steal a Brainrot
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/Steal%20A%20Brainrot"))();
-    loadscript()
 else
     task.wait(2)
     game.Players.LocalPlayer:Kick("Not Supported PlaceId: " .. game.PlaceId .. "\nPlease use the correct game to run this script.")
