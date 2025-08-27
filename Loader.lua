310e807a80b64c2b5a6458e6cf663c4510457c26
Ghost <jirapat2957@gmail.com>
2025-08-27 08:58:48 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
 
+
 if game.PlaceId == 126884695634066 or game.PlaceId == 91867617264223 then -- Grow a Garden
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/GrowaGarden"))();
 elseif game.PlaceId == 79546208627805 or game.PlaceId == 126509999114328 then -- 99 Nights in the Forest
@@ -9,14 +10,10 @@ elseif game.PlaceId == 129827112113663 then -- Prospecting
 elseif game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))();
 elseif game.GameId == 994732206 or game.GameId == 3756501373 then -- Blox Fruit & Meme Sea
-    local Settings = {
-        JoinTeam = "Pirates";
-        Translator = true;
-    }
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AAwful/Vector_Hub/0/v2"))()
 elseif game.GameId == 7709344486 then -- Steal a Brainrot
     loadstring(game:HttpGet("https://raw.githubusercontent.com/GhostSobe/Script/refs/heads/main/Steal%20A%20Brainrot"))();
 else
     task.wait(2)
-    game.Players.LocalPlayer:Kick("Not Supported PlaceId: " .. game.PlaceId .. "\nPlease use the correct game to run this script.")
+    game.Players.LocalPlayer:Kick("ไม่ Support PlaceId: " .. game.PlaceId .. "\nโปรดใช้เกมที่รับรอง เพื่อเรียกใช้สคริปต์")
 end
